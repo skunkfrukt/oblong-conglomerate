2581c85401bfeee9663c8e79d9cbeae34ad08d6b
Namida Aneskans <namida@skunkfrukt.se>
2014-01-21 19:06:33 +0100

    Crude but working TextScene implementation

--- src/scene.py
+++ src/scene.py
@@ -10,17 +10,30 @@ class Scene(object):
 
 
 class TextScene(Scene):
-    def __init__(self, text, font, next):
+    def __init__(self, next, text, font, speed=10, final_delay=None):
         self.text = text
         self.next = next
+        self.speed = speed
+        self.font = font
+        self.final_delay = final_delay
         self.time = 0
+        self.sprites = []
+
+    def setup(self):
+        self.time = 0
+        self.sprites = self.font.render(self.text)
+
 
     def update(self, dt):
         self.time += dt
-        self.update_text()
+        if self.final_delay is not None:
+            current_delay = self.time - len(self.sprites) / float(self.speed)
+            if current_delay >= self.final_delay:
+                return 'DONE'
 
-    def update_text(self):
-        pass
+    def draw(self):
+        for spr in self.sprites[:int(self.time*self.speed)]:
+            spr.draw()
 
 
 class GameScene(Scene):
