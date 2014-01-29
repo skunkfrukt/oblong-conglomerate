70d1c29385ffd9085c93aab8d0f56df81cf43392
Namida Aneskans <namida@skunkfrukt.se>
2014-01-29 20:57:43 +0100

    Moved collision code into GameScene class and made a test for it

--- src/scene.py
+++ src/scene.py
@@ -1,4 +1,6 @@
 import pyglet
+from shapes import *
+import knightexpert
 
 
 class Scene(object):
@@ -21,36 +23,103 @@ class TextScene(Scene):
 
     def setup(self):
         self.time = 0
+        self.done = False
         self.sprites = self.font.render(self.text)
 
 
-    def update(self, dt):
+    def mupdate(self, dt):
+        if self.done:
+            return 'next'
         self.time += dt
         if self.final_delay is not None:
             current_delay = self.time - len(self.sprites) / float(self.speed)
             if current_delay >= self.final_delay:
-                return 'DONE'
+                self.done = True
+
+    def vupdate(self):
+        pass
 
     def draw(self):
         for spr in self.sprites[:int(self.time*self.speed)]:
             spr.draw()
 
+    def on_key_press(self, symbol, modifiers):
+        self.done = True
 
-class GameScene(Scene):
-    def __init__(self, tilemap):
-        self.tilemap = tilemap
-        self.backgrounds = []
-        self.sprites = []
-
-    def setup(self):
-        for tset in self.tilemap.tilesets:
-            pass
-
-    def update(self, dt):
+    def on_key_release(self, symbol, modifiers):
         pass
 
+
+class GameScene(Scene):
+    def __init__(self, tilemap, tilemapview, hero_position):
+        self.tilemap = tilemap
+        self.tilemapview = tilemapview
+        self.hero = knightexpert.KnightExpert()
+        self.hero.hitbox.position = hero_position
+        self.heroview = knightexpert.KnightExpertView(self.hero)
+        self.herocontroller = knightexpert.KnightExpertController(self.hero)
+
+    def setup(self):
+        self.tilemap.setup()
+        self.tilemapview.setup()
+
+    def mupdate(self, dt):
+        if self.hero.hitbox.x > 256:
+            return 'next'
+        self.hero.start_move(dt)
+        delta = self.hero.velocity * dt
+        rows = range(int(self.hero.hitbox.bottom // self.tilemap.tileheight),
+                int(self.hero.hitbox.top // self.tilemap.tileheight) + 1)
+        if delta.x < 0:
+            cols = range(int((self.hero.hitbox.left - 1) // self.tilemap.tilewidth),
+                    int((self.hero.hitbox.left + delta.x) // self.tilemap.tilewidth) - 1, -1)
+        elif delta.x > 0:
+            cols = range(int((self.hero.hitbox.right + 1) // self.tilemap.tilewidth),
+                    int((self.hero.hitbox.right + delta.x) // self.tilemap.tilewidth) + 1)
+        else:
+            cols = []
+        candidates_x = [(col, row) for col in cols for row in rows]
+        hit_x = self.tilemap.find_obstacle(candidates_x)
+        if hit_x:
+            self.hero.velocity = Vect(0, self.hero.velocity.y)
+            if delta.x < 0:
+                delta = Vect(hit_x.right + 1 - self.hero.hitbox.left, delta.y)
+            elif delta.x > 0:
+                delta = Vect(hit_x.left - 1 - self.hero.hitbox.right, delta.y)
+        cols = range(int((self.hero.hitbox.left + delta.x) // self.tilemap.tilewidth),
+                int((self.hero.hitbox.right + delta.x) // self.tilemap.tilewidth) + 1)
+        if delta.y < 0:
+            rows = range(int((self.hero.hitbox.bottom - 1) // self.tilemap.tileheight),
+                    int((self.hero.hitbox.bottom + delta.y) // self.tilemap.tileheight) - 1, -1)
+        elif delta.y > 0:
+            rows = range(int((self.hero.hitbox.top + 1) // self.tilemap.tileheight),
+                    int((self.hero.hitbox.top + delta.y) // self.tilemap.tileheight) + 1)
+        candidates_y = [(col, row) for row in rows for col in cols]
+        hit_y = self.tilemap.find_obstacle(candidates_y)
+        if hit_y:
+            self.hero.velocity = Vect(self.hero.velocity.x, 0)
+            if delta.y < 0:
+                delta = Vect(delta.x, hit_y.top + 1 - self.hero.hitbox.bottom)
+                if delta.x == 0:
+                    self.hero.state = 'idle'
+                else:
+                    self.hero.state = 'walking'
+            elif delta.y > 0:
+                delta = Vect(delta.x, hit_y.bottom - 1 - self.hero.hitbox.top)
+        self.hero.hitbox += delta
+
+    def vupdate(self):
+        player_position = self.hero.hitbox.x
+        screen_offset = int(-max(min(player_position - 32, 256 - 64), 0))
+        self.tilemapview.update_sprite_position(Vect(x=screen_offset))
+        self.heroview.update_sprite_position(Vect(screen_offset,0))
+
     def draw(self):
-        for bg in self.backgrounds:
-            bg.draw()
-        for spr in self.sprites:
-            spr.draw()
+        self.tilemapview.batch.draw()
+        self.heroview.drawable.draw()
+
+    def on_key_press(self, symbol, modifiers):
+        self.herocontroller.on_key_press(symbol, modifiers)
+
+    def on_key_release(self, symbol, modifiers):
+        self.herocontroller.on_key_release(symbol, modifiers)
---
+++ test_scene.py
@@ -0,0 +1,165 @@
+import pyglet
+from pyglet.gl import *
+from src import tilemap, scene, text
+from pyglet.window import key
+from src import knightexpert, collider
+from src.shapes import *
+import json
+
+pyglet.resource.path.append('./data')
+pyglet.resource.reindex()
+
+SCALE = 4
+
+f_bindings = {
+    'a': 16*5+0,
+    'b': 16*5+1,
+    'c': 16*5+2,
+    'd': 16*5+3,
+    'e': 16*5+4,
+    'f': 16*5+5,
+    'g': 16*5+6,
+    'h': 16*5+7,
+    'i': 16*5+8,
+    'j': 16*5+9,
+    'k': 16*5+10,
+    'l': 16*5+11,
+    'm': 16*5+12,
+    'n': 16*5+13,
+    'o': 16*5+14,
+    'p': 16*5+15,
+
+    'q': 16*4+0,
+    'r': 16*4+1,
+    's': 16*4+2,
+    't': 16*4+3,
+    'u': 16*4+4,
+    'v': 16*4+5,
+    'w': 16*4+6,
+    'x': 16*4+7,
+    'y': 16*4+8,
+    'z': 16*4+9,
+    '.': 16*4+10,
+    ',': 16*4+11,
+    ':': 16*4+12,
+    ';': 16*4+13,
+    '!': 16*4+14,
+    '?': 16*4+15,
+
+    '(': 16*3+0,
+    ')': 16*3+1,
+    '[': 16*3+2,
+    ']': 16*3+3,
+    '{': 16*3+4,
+    '}': 16*3+5,
+    '_': 16*3+6,
+    '\\': 16*3+7,
+    '|': 16*3+8,
+    '&': 16*3+9,
+    '+': 16*3+10,
+    '-': 16*3+11,
+    '*': 16*3+12,
+    '/': 16*3+13,
+    '"': 16*3+14,
+    '\'': 16*3+15,
+
+    '=': 16*2+0,
+    '<': 16*2+1,
+    '>': 16*2+2,
+
+    '0': 0,
+    '1': 1,
+    '2': 2,
+    '3': 3,
+    '4': 4,
+    '5': 5,
+    '6': 6,
+    '7': 7,
+    '8': 8,
+    '9': 9
+}
+
+font = text.Font('data/font.png', 16, 6, f_bindings)
+texts_raw = json.load(pyglet.resource.file('data/texts.json'))['texts']
+texts = {}
+for t in texts_raw:
+    texts[t['name']] = t['text']
+
+opening_1 = scene.TextScene(None, texts['opening-1'], font)
+opening_2 = scene.TextScene(None, texts['opening-2'], font)
+opening_3 = scene.TextScene(None, texts['opening-3'], font)
+title = scene.TextScene(None, texts['title'], font)
+town_0 = scene.GameScene(*tilemap.load('data/m_town.json'), hero_position=Vect(100, 4))
+stage1_0 = scene.GameScene(*tilemap.load('data/m_stage1.json'), hero_position=Vect(4, 4))
+intermission1_2_0 = scene.TextScene(None, texts['intermission-1to2'], font)
+stage2_0 = scene.GameScene(*tilemap.load('data/m_stage2.json'), hero_position=Vect(4, 36))
+intermission2_3_0 = scene.TextScene(None, texts['intermission-2to3'], font)
+stage3_0 = scene.GameScene(*tilemap.load('data/m_stage3.json'), hero_position=Vect(244, 8))
+intermission3_2_1 = scene.TextScene(None, texts['intermission-3to2+'], font)
+stage2_1 = scene.GameScene(*tilemap.load('data/m_stage2_n.json'), hero_position=Vect(248, 48))
+intermission2_1_1 = scene.TextScene(None, texts['intermission-2+to1+'], font)
+stage1_1 = scene.GameScene(*tilemap.load('data/m_stage1_n.json'), hero_position=Vect(248, 36))
+town_1 = scene.GameScene(*tilemap.load('data/m_town_n.json'), hero_position=Vect(248, 4))
+stage1_2 = scene.GameScene(*tilemap.load('data/m_stage1_n.json'), hero_position=Vect(4, 4))
+intermission1_2_2 = scene.TextScene(None, texts['intermission-1++to2++'], font)
+stage2_2 = scene.GameScene(*tilemap.load('data/m_stage2_n.json'), hero_position=Vect(4, 36))
+intermission2_3_2 = scene.TextScene(None, texts['intermission-2++to3++'], font)
+stage3_2 = scene.GameScene(*tilemap.load('data/m_stage3_n.json'), hero_position=Vect(244, 8))
+stage2_3 = scene.GameScene(*tilemap.load('data/m_stage2.json'), hero_position=Vect(248, 48))
+stage1_3 = scene.GameScene(*tilemap.load('data/m_stage1.json'), hero_position=Vect(248, 36))
+triumphant_return = scene.TextScene(None, texts['triumphant-return'], font)
+town_2 = scene.GameScene(*tilemap.load('data/m_town.json'), hero_position=Vect(248, 4))
+goodending_0 = scene.TextScene(None, texts['goodEnding-0'], font)
+goodending_1 = scene.TextScene(None, texts['goodEnding-1'], font)
+goodending_2 = scene.TextScene(None, texts['goodEnding-2'], font)
+goodending_3 = scene.TextScene(None, texts['goodEnding-3'], font)
+anyending_0 = scene.TextScene(None, texts['anyEnding-0'], font)
+anyending_1 = scene.TextScene(None, texts['anyEnding-1'], font)
+dotdotdot = scene.TextScene(None, texts['dotDotDot'], font)
+anyending_2 = scene.TextScene(None, texts['anyEnding-2'], font)
+credits_0 = scene.TextScene(None, texts['credits-0'], font)
+credits_1 = scene.TextScene(None, texts['credits-1'], font)
+
+SCENES = [opening_1, opening_2, opening_3, title,
+        town_0, stage1_0, intermission1_2_0, stage2_0, intermission2_3_0, stage3_0,
+        intermission3_2_1, stage2_1, intermission2_1_1, stage1_1,
+        town_1, stage1_2, intermission1_2_2, stage2_2, intermission2_3_2, stage3_2,
+        stage2_3, stage1_3, triumphant_return, town_2,
+        goodending_0, goodending_1, goodending_2, goodending_3,
+        anyending_0, anyending_1, dotdotdot, anyending_2,
+        credits_0, credits_1]
+
+class TestWindow(pyglet.window.Window):
+    def __init__(self):
+        super(TestWindow, self).__init__(64*SCALE, 48*SCALE, caption="\'blong Cong")
+        testmap = tilemap.load('data/m_stage1.json')
+        map, mapv = testmap
+        self.scene = SCENES.pop(0)
+        self.scene.setup()
+
+    def mupdate(self, dt):
+        if self.scene.mupdate(dt) == 'next':
+            self.scene = SCENES.pop(0)
+            self.scene.setup()
+
+    def on_draw(self):
+        self.clear()
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST)
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST)
+        self.scene.vupdate()
+        self.scene.draw()
+
+    def on_key_press(self, symbol, modifiers):
+        self.scene.on_key_press(symbol, modifiers)
+
+    def on_key_release(self, symbol, modifiers):
+        self.scene.on_key_release(symbol, modifiers)
+
+
+w = TestWindow()
+glScalef(SCALE, SCALE, SCALE)
+
+
+
+pyglet.clock.schedule_interval(w.mupdate, 0.02)
+pyglet.app.run()
